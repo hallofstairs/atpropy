4e38b52c0d8cb95de04a86337b7b589c16f03d15
Jett Hollister <jett.hollister@duke.edu>
2023-04-09 12:39:37 -0400

    Added Thread and Actor types

--- protopy/types.py
+++ protopy/types.py
@@ -1,4 +1,6 @@
-from typing import Optional
+from __future__ import annotations
+
+from typing import Literal, Optional
 
 from pydantic import BaseModel, Field
 
@@ -19,14 +21,20 @@ class Viewer(BaseModel):
 
 
 class Actor(BaseModel):
-    pass
+    did: str
+    handle: str
+    displayName: Optional[str]
+    description: Optional[str]
+    avatar: Optional[str]
+    indexedAt: str
+    viewer: Viewer
 
 
 class Author(BaseModel):
     did: str
     handle: str
-    displayName: str
-    avatar: str
+    displayName: Optional[str]
+    avatar: Optional[str]
     viewer: Viewer
 
 
@@ -50,23 +58,60 @@ class Index(BaseModel):
     byteStart: int
 
 
-class Feature(BaseModel):
+class MentionFeature(BaseModel):
+    type: Literal["app.bsky.richtext.facet#mention"] = Field(..., alias="$type")
     did: str
-    type_field: str = Field(..., alias="$type")
+
+
+class LinkFeature(BaseModel):
+    type: Literal["app.bsky.richtext.facet#link"] = Field(..., alias="$type")
 
 
 class Facet(BaseModel):
-    type_field: str = Field(..., alias="$type")
+    type: Optional[Literal["app.bsky.richtext.facet"]] = Field(None, alias="$type")
     index: Index
-    features: list[Feature]
+    features: list[MentionFeature | LinkFeature]
+
+
+class RecordImageRef(BaseModel):
+    link: str = Field(..., alias="$link")
+
+
+class RecordImage(BaseModel):
+    type: Literal["blob"] = Field(..., alias="$type")
+    ref: RecordImageRef
+    mimeType: Literal["image/jpeg"] | str  # TODO: This should only be valid image types
+    size: int
+
+
+class RecordImageData(BaseModel):
+    alt: str
+    image: RecordImage
+
+
+class RecordImageEmbed(BaseModel):
+    type: Literal["app.bsky.embed.images"] = Field(..., alias="$type")
+    images: list[RecordImageData]
 
 
 class Record(BaseModel):
+    type: Literal["app.bsky.feed.post"] = Field(..., alias="$type")
     text: str
-    type_field: str = Field(..., alias="$type")
     createdAt: str  # datetime
     reply: Optional[ReplyId]
     facets: Optional[list[Facet]]
+    embed: Optional[RecordImageEmbed]  # TODO: Add other types of embeds eg video
+
+
+class Image(BaseModel):
+    thumb: str
+    fullsize: str
+    alt: str
+
+
+class Embed(BaseModel):
+    type: Literal["app.bsky.embed.images#view"] = Field(..., alias="$type")
+    images: list[Image]
 
 
 class Post(BaseModel):
@@ -79,6 +124,7 @@ class Post(BaseModel):
     likeCount: int
     indexedAt: str
     viewer: Viewer
+    embed: Optional[Embed]
 
 
 class Reply(BaseModel):
@@ -94,3 +140,27 @@ class FeedItem(BaseModel):
 class Feed(BaseModel):
     feed: list[FeedItem]
     cursor: str
+
+
+class Like(BaseModel):
+    createdAt: str
+    indexedAt: str
+    actor: Actor
+
+
+class Likes(BaseModel):
+    uri: str
+    cid: str
+    cursor: str
+    likes: list[Like]
+
+
+class ThreadPost(BaseModel):
+    type: Literal["app.bsky.feed.defs#threadViewPost"] = Field(..., alias="$type")
+    post: Post
+    parent: Optional[ThreadPost]
+    replies: list[ThreadPost]
+
+
+class Thread(BaseModel):
+    thread: ThreadPost  # Recursive dict, not list
